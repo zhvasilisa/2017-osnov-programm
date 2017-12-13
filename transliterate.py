cd3b97e7c209c237c4424fe4de5125a87daccc0a
zhvasilisa <zhvasilisa@gmail.com>
2017-12-13 17:20:36 +0100

    corrected transliterator

--- transliterate.py
+++ transliterate.py
@@ -9,7 +9,9 @@ for line in tr.readlines():
 	lettersdict[l] = c
 
 for line in sys.stdin.readlines():
+	line = line.strip()
 	if '\t' not in line:
+		print(line)
 		continue
 	row = line.split('\t')
 	if len(row) != 10:
@@ -19,12 +21,7 @@ for line in sys.stdin.readlines():
 	row[9] = 'Translit='
 	for l in lettersdict:
 		newform = newform.replace(l, lettersdict[l])
-	row[9] = row[9] + newform
+	row[9] = row[9] + newform.strip('#')
 	print('\t'.join(row))
+
 	
-#for symbol in form:
-#		if symbol in lettersdict:
-#			row[9] = row[9] + lettersdict[symbol]
-#		else:
-#			row[9] = row[9] + symbol
-#	print('\t'.join(row))
